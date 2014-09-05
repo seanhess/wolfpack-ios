d47068b1f942a1268a99c1c75825da44ecd2f357
Sean Clark Hess <seanhess@gmail.com>
2014-09-04 21:44:06 -0600

    initial commit

---
+++ Wolf Pack/Wolf Pack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wolf Pack
+//
+//  Created by Sean Hess on 9/4/14.
+//  Copyright (c) 2014 Orbital Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
