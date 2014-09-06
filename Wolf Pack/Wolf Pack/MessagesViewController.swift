a85991c5086b487b5a24db67d67ff3d617858d41
taterbase <shankga@gmail.com>
2014-09-06 04:02:56 -0600

    Beginning of messaging views

---
+++ Wolf Pack/Wolf Pack/MessagesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MessagesViewController.swift
+//  Wolf Pack
+//
+//  Created by George Shank on 9/6/14.
+//  Copyright (c) 2014 Orbital Labs. All rights reserved.
+//
+
+import UIKit
+
+class MessagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
